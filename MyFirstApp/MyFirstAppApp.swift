ce8977b920b33dd1d610784fd0c8f8f3f5815566
lui <lukeingalls@me.com>
2022-11-13 16:25:09 -0700

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Luke Ingalls on 11/13/22.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
