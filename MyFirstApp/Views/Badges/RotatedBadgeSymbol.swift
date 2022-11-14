fa718c0739aa41119962cee829495117925ecad6
lui <lukeingalls@me.com>
2022-11-14 08:07:44 -0700

    completed drawign paths and shapres

---
+++ MyFirstApp/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  MyFirstApp
+//
+//  Created by Luke Ingalls on 11/14/22.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
