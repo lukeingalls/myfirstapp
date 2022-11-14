56497db9da89ecf2e28bb4e929fe55c3fa2a432a
lui <lukeingalls@me.com>
2022-11-13 20:54:22 -0700

    complete stage 3 of tutorial

---
+++ MyFirstApp/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MyFirstApp
+//
+//  Created by Luke Ingalls on 11/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList();
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
